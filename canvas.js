1d6ff901edae1b29c0073e21e48e147df9fb29b5
William Kang <Willipai@pal-nat186-52-51.itap.purdue.edu>
2017-12-04 00:54:34 -0500

    so many circles

--- canvas.js
+++ canvas.js
@@ -5,18 +5,61 @@ canvas.height = window.innerHeight;
 
 let c = canvas.getContext('2d');
 
-let x = 200;
-let y = 200;
+function random_rgba() {
+    var o = Math.round, r = Math.random, s = 255;
+    return 'rgba(' + o(r()*s) + ',' + o(r()*s) + ',' + o(r()*s) + ',' + r().toFixed(1) + ')';
+}
+
+function Circle(x, y, dx, dy, radius){
+    this.x = x;
+    this.y = y;
+    this. dx = dx;
+    this.dy = dy;
+    this.radius = radius;
+
+    this.draw = function(){
+        c.beginPath();
+        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        let color = random_rgba();
+        c.strokeStyle = color;
+        c.fillStyle = color;
+        c.stroke();
+        c.fill()
+    }
+
+    this.update = function(){
+        if(this.x + this.radius > innerWidth || this.x - this.radius < 0){
+            this.dx = -this.dx;
+        }
+    
+        if(this.y + this.radius > innerHeight || this.y - this.radius < 0){
+            this.dy = -this.dy;
+        }
+        this.x += this.dx;
+        this.y += this.dy;
+        this.draw();
+    }
+}
+    
+let circleArray = [];
+
+for(let i = 0; i < 100; i++){
+    let radius = 30;
+    let x = Math.random() * (innerWidth - radius * 2) + radius;
+    let y = Math.random() * (innerHeight - radius * 2) + radius;
+    let dx = (Math.random() - .05) * 10;
+    let dy= (Math.random() - .05) * 10;
+    circleArray.push(new Circle(x, y, dx, dy, radius));
+}
+
 function animate(){
     requestAnimationFrame(animate);
-
     c.clearRect(0, 0, innerWidth, innerHeight);
-    c.beginPath();
-    c.arc(x, y, 30, 0, Math.PI * 2, false);
-    c.stroke();
 
-    x+=1;
-    y+=1;
+    for (let index = 0; index < circleArray.length; index++) {
+        circleArray[index].update();
+        
+    }
 }
 
 animate();
